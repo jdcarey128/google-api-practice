877d4b27a4a962cf9649a573936dc0a27a28bc8e
Joshua Carey <jdcarey128@gmail.com>
2021-05-11 16:37:04 -0600

    silence omniauth warnings

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   OmniAuth.config.allowed_request_methods = [:get]
+  OmniAuth.config.silence_get_warning = true
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
 end
