9d174280770eb97813ea5528eff18c58cb2cd4bc
Joshua Carey <jdcarey128@gmail.com>
2021-05-11 16:35:59 -0600

    Add logout method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,4 +9,10 @@ class SessionsController < ApplicationController
     
     redirect_to dashboard_path
   end
+
+  def destroy 
+    # delete session data 
+    reset_session()
+    redirect_to root_path
+  end
 end
