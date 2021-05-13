b1307b485ac31043063e731a5340d0c0bb461870
Joshua Carey <jdcarey128@gmail.com>
2021-05-13 17:59:50 -0600

    Add data service to access sheet values

--- app/services/data_service.rb
+++ app/services/data_service.rb
@@ -1,7 +1,18 @@
+require 'google/apis/sheets_v4'
+
 class DataService 
   class << self 
     def get_data(token)
-      require 'pry'; binding.pry
+      spreadsheet_id = ENV['GOOGLE_SPREADSHEET_ID']
+
+      service = Google::Apis::SheetsV4::SheetsService.new()
+      service.authorization = token 
+      begin
+        response = service.get_spreadsheet_values("#{spreadsheet_id}", "A1:G5")
+      rescue => e
+        require 'pry'; binding.pry
+      end
+
     end
   end
 end
