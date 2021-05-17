235a18875813720c347fff85f537fc09870d8098
Joshua Carey <jdcarey128@gmail.com>
2021-05-17 11:02:48 -0600

    Add items_from_sheets method

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,19 @@
 class Item < ApplicationRecord
   validates :name, :description, :unit_price, :school, :merchant_id, presence: true
+  class << self 
+    def items_from_sheets(school_items)
+      school_items.each do |items_values|
+        items_values.values[1..-1].each do |values|
+          Item.find_or_create_by(
+            id: values[0],
+            name: values[1],
+            description: values[2],
+            unit_price: values[3].to_f,
+            merchant_id: values[4].to_i,
+            school: items_values.school
+          )
+        end
+      end
+    end
+  end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -8,4 +8,34 @@ RSpec.describe Item, type: :model do
     it { should validate_presence_of :merchant_id }
     it { should validate_presence_of :school }
   end
+
+  describe 'methods' do 
+    describe 'create_items_from_sheets' do 
+      before :each do 
+        sheet_data = JSON.parse(File.read('spec/fixtures/google_sheet_data.json'), symbolize_names: true)
+        @items = create_open_struct(sheet_data[:results])
+      end
+      
+      it 'should create items from sheets' do 
+        expect(Item.count).to eq(0)
+        Item.items_from_sheets(@items)
+        expect(Item.count).to eq(12)
+
+        item_5 = Item.find_by(id: 5)
+        expect(item_5.name).to eq("Item Expedita Aliquam")
+        expect(item_5.unit_price).to eq(68723)
+        expect(item_5.school).to eq('school-1')
+
+        item_54 = Item.find_by(id: 54)
+        expect(item_54.school).to eq('school-2')
+      end
+
+      it 'should find items if they have already been created' do 
+        Item.items_from_sheets(@items)
+        expect(Item.count).to eq(12)
+        Item.items_from_sheets(@items)
+        expect(Item.count).to eq(12)
+      end
+    end
+  end
 end
