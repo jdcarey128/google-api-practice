9defdfc457ffcb7eac17f9fc84fd315255deb59c
Joshua Carey <jdcarey128@gmail.com>
2021-05-18 16:01:41 -0600

    Add Download class for generating pdfs

---
+++ app/poros/download.rb
@@ -0,0 +1,26 @@
+require 'render_anywhere'
+
+class Download 
+  include RenderAnywhere
+
+  def initialize(report)
+    @report = report 
+  end
+
+  def to_pdf
+    kit = PDFKit.new(as_html)
+    kit.to_file('tmp/report.pdf')
+  end
+
+  def filename
+    "Report #{report.id}.pdf"
+  end
+
+  private 
+  attr_reader :report 
+
+  def as_html 
+    render template: "reports/pdf",
+           locals: { report: report }
+  end
+end
