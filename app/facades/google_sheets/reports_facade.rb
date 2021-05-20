c56530ad258e5c3082ffad554e4b1582f4164a99
Joshua Carey <jdcarey128@gmail.com>
2021-05-20 08:33:57 -0600

    Add user_id argument for fetch google sheets data

--- app/facades/google_sheets/reports_facade.rb
+++ app/facades/google_sheets/reports_facade.rb
@@ -1,13 +1,13 @@
 module GoogleSheets
   class ReportsFacade
     class << self 
-      def fetch_sheet_data(token, count=10)
+      def fetch_sheet_data(token, count=10, user_id)
         response = GoogleSheets::ReportsService.fetch_sheet_data(token, count)
         # returns array of item ids 
         item_ids = Item.items_from_sheets(response)
         # returns created report 
         # links report to Items through Report Items 
-        Report.generate_report(item_ids)
+        Report.generate_report(user_id, item_ids)
       end
     end
   end
